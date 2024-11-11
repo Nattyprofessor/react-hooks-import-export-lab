32a37cd6f1c731c5551c41db3431664e91426d8d
Anthony Kabuthu <kabuthuantony@gmail.com>
2024-11-11 13:19:19 +0300

    Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "./NavBar";  // Correct import (same directory)
+import Home from "./Home";      // Correct import (same directory)
+import About from "./About";    // Correct import (same directory)
 
 function App() {
   return (
